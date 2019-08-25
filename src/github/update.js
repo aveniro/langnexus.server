8b8f7d242d2c503f2b8011d699d223183e404a12
Nathan Seymour <nathan@seymour.global>
2019-08-24 23:15:01 -0400

    Switch to spawn instead of stupid nodegit

--- src/github/update.js
+++ src/github/update.js
@@ -2,44 +2,28 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const nodemailerConfig = require('../../nodemailer.config.json');
 const config = require('../../config.json');
-const nodegit = require('nodegit');
-const path = require('path');
+const { spawn } = require('child_process');
 
 const update = express.Router();
 
 update.post('/', (req, res) => {
     if (req.body.action === 'closed' && req.body.pull_request.merged) {
         // Get the new commits
-        let repository;
 
-        nodegit.Repository.open(config.langnexusPath)
-            .then((repo) => {
-                repository = repo;
-
-                return repository.fetchAll({
-                    callbacks: {
-                        credentials(url, userName) {
-                            return nodegit.Cred.sshKeyFromAgent(userName);
-                        },
-                        certificateCheck() {
-                            return 0;
-                        }
-                    }
-                });
-            })
-            .then(function () {
-                return repository.mergeBranches("production", "origin/production");
-            })
-            .done(function () {
-                let transporter = nodemailer.createTransport(nodemailerConfig.transporter);
-
-                let info = transporter.sendMail({
-                    from: '"Langnexus" <noreply@langnexus.io>', // sender address
-                    to: config.sysadmin, // list of receivers
-                    subject: 'Langnexus has been successfully updated.', // Subject line
-                    text: 'this' // plain text body
-                });
+        spawn('git', ['pull'], { cwd: config.langnexusPath })
+            .on('close', () => {
+                spawn('npm', ['run-script', 'build'], { cwd: config.langnexusPath });
             });
+
+
+        let transporter = nodemailer.createTransport(nodemailerConfig.transporter);
+
+        let info = transporter.sendMail({
+            from: '"Langnexus" <noreply@langnexus.io>', // sender address
+            to: config.sysadmin, // list of receivers
+            subject: 'Langnexus has been successfully updated.', // Subject line
+            text: 'this' // plain text body
+        });
     }
 
     res.send();
